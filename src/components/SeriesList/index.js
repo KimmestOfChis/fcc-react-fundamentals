cffd85915ee42663d997a920d6213875b4ba9cfc
Matthew Johnson <mjohnson@scriptdrop.co>
2019-03-06 17:50:41 -0500

    Using Route Params

--- src/components/SeriesList/index.js
+++ src/components/SeriesList/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './index.css'
 
 const SeriesListItem = ({ series }) => (
     <li>
-        {series.show.name}
+        <Link to={`/series/${series.show.id}`}>
+            {series.show.name}
+        </Link>
     </li>
 )
 
--- src/components/SingleSeries/index.js
+++ src/components/SingleSeries/index.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import Loader from '../Loader';
 
 class SingleSeries extends Component {
+    state = {
+        show: null
+    }
+
+    componentDidMount() {
+        const { id } = this.props.match.params;
+        fetch(`http://api.tvmaze.com/shows/${id}?embed=episodes`)
+        .then((response) => response.json())
+        .then(json => this.setState({ show: json }))
+    }
     render() {
+        const { show } = this.state;
     return(
         <div>
-            <p>Signle Series - the show id will be {this.props.match.params.id} </p>
+            { show === null && <Loader /> }
+            {
+                show !== null
+                &&
+                <div>
+                    <p>{show.name}</p>
+                    <p>Premiered - {show.premiered}</p>
+                    <p>Rating - {show.rating.average}</p>
+                    <p>Episode - {show._embedded.episodes.length}</p>
+                    <p>
+                        <img alt="Show" img={show.image.medium}/>
+                    </p>
+                </div>
+            }
         </div>
         )
     }
