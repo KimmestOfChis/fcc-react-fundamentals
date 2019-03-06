82b90be534bb0b33e293ad86430f3773d667ffc8
Matthew Johnson <mjohnson@scriptdrop.co>
2019-03-06 16:37:30 -0500

    Refactoring List Component

--- src/components/SeriesList/index.js
+++ src/components/SeriesList/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './index.css'
 
+const SeriesListItem = ({ series }) => (
+    <li key={series.show.id}>
+        {series.show.name}
+    </li>
+)
+
 const SeriesList = (props) => {
     return(
         <div>
             <ul className="series-list">
                 {props.list.map(series => (
-                    <li key={series.show.id}>{series.show.name}</li>
+                    <SeriesListItem series={series} />
                 ))}
             </ul>
         </div>
