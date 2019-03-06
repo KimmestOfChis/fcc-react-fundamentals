6efe27c034b9a06ea224ee4f90ed3db43c845961
Matthew Johnson <mjohnson@scriptdrop.co>
2019-03-06 16:51:24 -0500

    Conditional Rendering

--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -4,22 +4,47 @@ import SeriesList from '../../components/SeriesList'
 class Series extends Component {
 
     state = {
-        series: []
+        series: [],
+        seriesName: '',
+        isFetching: false
       }
 
       onSeriesInputChange = e => {
+        this.setState({ seriesName: e.target.value, isFetching: true });
+
         fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
         .then((response) => response.json())
-        .then(json => this.setState({ series: json }))
+        .then(json => this.setState({ series: json, isFetching: false }))
       }
 
     render() {
+        const { series, seriesName, isFetching } = this.state;
+
         return(
             <div>
                 <div>
-                    <input type="text" onChange={this.onSeriesInputChange} />
+                    <input 
+                        value={seriesName}
+                        type="text" 
+                        onChange={this.onSeriesInputChange} 
+                    />
                 </div>
-                <SeriesList list={this.state.series} />
+                { 
+                    series.length === 0  && seriesName.trim() === ''
+                 && 
+                <p>Please enter series name into the input</p>
+                }
+                {
+                    series.length === 0 && seriesName.trim() !== ''
+                    &&
+                    <p>No TV series have been found with this name</p>
+                }
+                {
+                    isFetching && <p>Loading...</p>
+                }
+                {
+                    !isFetching &&  <SeriesList list={this.state.series} />
+                }
             </div>
         )
     }
