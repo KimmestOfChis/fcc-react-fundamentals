dfb8d350323391e0cfb170449a9fe70ff64c9748
Matthew Johnson <mjohnson@scriptdrop.co>
2019-03-06 17:33:40 -0500

    Setting Routes

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Series from '../../containers/Series';
+import SingleSeries from '../../components/SingleSeries';
 
 const Main = props => (
     <Switch>
         <Route exact path="/" component={Series} />
+        <Route path="/series/:id" component={SingleSeries} />
     </Switch>
 );
 
